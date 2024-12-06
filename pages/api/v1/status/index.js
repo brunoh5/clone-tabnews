438bb9278c23cb432cd9d8c6c4bfd4a2900d8ded
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-12-05 22:35:51 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+
+  console.log(result.rows);
+
   return response
     .status(200)
     .json({ chave: "alunos do curso.dev são pessoas acima da média" });
