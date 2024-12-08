4b0a2fa188113deb19e0da77c145a5b702f0bb0d
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-12-08 12:07:54 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const databaseVersionResult = await database.query(`SHOW server_version;`);
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  return response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+  const databaseMaxConnectiosResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectiosResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SElECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
