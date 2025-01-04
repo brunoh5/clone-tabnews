4b77113662d287b90055e284648fa0c8488f5aca
Bruno Henrique <brunohenriquesantos272@gmail.com>
2025-01-04 12:31:02 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/not-allowed.spec.js
+++ tests/integration/api/v1/migrations/not-allowed.spec.js
@@ -1,23 +1,38 @@
-test("DELETE to /api/v1/migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
+describe("Not Allowed /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Return not allowed for differents methods", () => {
+      test("DELETE", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "DELETE",
+          },
+        );
+
+        expect(response.status).toBe(405);
+      });
+
+      test("PUT", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "PUT",
+          },
+        );
+
+        expect(response.status).toBe(405);
+      });
+
+      test("PATCH", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "PATCH",
+          },
+        );
+
+        expect(response.status).toBe(405);
+      });
+    });
   });
-
-  expect(response.status).toBe(405);
-});
-
-test("PUT to /api/v1/migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PUT",
-  });
-
-  expect(response.status).toBe(405);
-});
-
-test("PATCH to /api/v1/migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PATCH",
-  });
-
-  expect(response.status).toBe(405);
 });
